0d801fd1f975392a14483f3065e2cc54292d1ea3
BLyndon <bheil@mail.de>
2024-02-18 13:35:04 +0100

    rename config for target file extension

--- config/config.go
+++ config/config.go
@@ -5,21 +5,21 @@ import (
 )
 
 type Config struct {
-	OpenAIModel   string
-	MaxTokenCount int
-	PromptFile    string
-	JsonExtension string
-	SourceDir     string
-	TargetDir     string
+	OpenAIModel         string
+	MaxTokenCount       int
+	PromptFile          string
+	TargetFileExtension string
+	SourceDir           string
+	TargetDir           string
 }
 
 func LoadConfig() Config {
 	return Config{
-		OpenAIModel:   openai.GPT3Dot5Turbo,
-		MaxTokenCount: 1000,
-		PromptFile:    "./assets/prompt.txt",
-		JsonExtension: ".json",
-		SourceDir:     "./data/sourceDir",
-		TargetDir:     "./data/targetDir/",
+		OpenAIModel:         openai.GPT3Dot5Turbo,
+		MaxTokenCount:       1000,
+		PromptFile:          "./assets/prompt.txt",
+		TargetFileExtension: ".json",
+		SourceDir:           "./data/sourceDir",
+		TargetDir:           "./data/targetDir",
 	}
 }
