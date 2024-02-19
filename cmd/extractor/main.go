bfc1ae781d69846cc0bb6520691da50cb25dc9f0
BLyndon <bheil@mail.de>
2024-02-19 10:16:32 +0100

    add cors middleware

--- cmd/extractor/main.go
+++ cmd/extractor/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"ebook-metadata-extractor/cmd/extractor/middleware"
 	"ebook-metadata-extractor/config"
 	"log"
 	"net/http"
@@ -8,10 +9,13 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/metadata/extract", ExtractMetaDataHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/metadata/extract", ExtractMetaDataHandler)
+
+	// Wrap the entire mux with the CORS middleware
+	handler := middleware.Cors(mux)
 
 	port := config.LoadConfig().Port
-
 	log.Println("Starting server on :" + strconv.Itoa(port))
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), handler))
 }
